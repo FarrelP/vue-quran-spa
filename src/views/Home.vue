3b116f4d6b86ff7de171015a70e16d2facfe89c3
FarrelP <31539444+FarrelP@users.noreply.github.com>
2021-06-21 15:01:53 +0700

    Update home views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="mt-4">
+      <div class="row">
+        <div class="col-lg-7 col-md-7">
+          <div class="d-flex justify-content-center">
+            <img src="../assets/images/bg-home.svg" alt="Home Background" height="300" width="50%">
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-5 mt-5">
+          <h5>Sudah baca <strong>Al Quran</strong> hari ini?</h5>
+          <br>
+          <h5>Rasulullah ﷺ bersabda: “Siapa saja membaca satu huruf dari Kitab Allah (Al Quran), maka baginya satu kebaikan, dan satu kebaikan itu dibalas dengan sepuluh kali lipatnya.” <br> (HR. At-Tirmidzi)</h5>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
